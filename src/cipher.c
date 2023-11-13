182b200544264699db6c574d7192abf6be7da304
Fook Athena <fookathe@ox-m4.msk.21-school.ru>
2023-11-13 18:10:09 +0300

    Done 2 quest

--- src/cipher.c
+++ src/cipher.c
@@ -15,9 +15,23 @@ int readAndPrintFile(const char *filename) {
     return 1;
 }
 
+int appendToFile(const char *filename, const char *text) {
+    FILE *file = fopen(filename, "a");
+    if (file == NULL) {
+        return 0;
+    }
+
+    fputs(text, file);
+    fputc('\n', file);
+    fclose(file);
+    return 1;
+}
+
 int main() {
     int choice = 0;
     char filename[256];
+    char text[256];
+    char test[100];
 
     while (choice != -1) {
         scanf("%d", &choice);
@@ -26,6 +40,22 @@ int main() {
                 (scanf("%255s", filename));
                 if (!readAndPrintFile(filename)) {
                     printf("n/a");
+                }
+                break;
+
+            case 2:
+                if (filename[0] == '\0') {
+                    scanf("%99s", test);
+                    printf("n/a");
+                } else {
+                    scanf(" %255[^\n]", text);
+                    if (!appendToFile(filename, text)) {
+                        printf("n/a");
+                        break;
+                    }
+                    if (!readAndPrintFile(filename)) {
+                        printf("n/a");
+                    }
                 }
                 break;
 
